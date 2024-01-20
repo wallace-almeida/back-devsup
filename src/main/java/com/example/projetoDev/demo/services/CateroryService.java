1bb8921cb4c83bf0ff8bfd1b90ec78abd53fd092
Wallace Ferreira <wallacefdealmeida@hotmail.com>
2024-01-20 15:58:23 -0300

    dataBase seeding

--- src/main/java/com/example/projetoDev/demo/services/CateroryService.java
+++ src/main/java/com/example/projetoDev/demo/services/CateroryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.projetoDev.demo.entities.Category;
 import com.example.projetoDev.demo.repositories.CategoryRepository;
@@ -13,6 +14,7 @@ public class CateroryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return repository.findAll();
     }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_category(nome)   VALUES ('Books');
+INSERT INTO tb_category(nome)   VALUES ('Carros');
+INSERT INTO tb_category(nome)   VALUES ('computer');
+INSERT INTO tb_category(nome)   VALUES ('notebooks');
+INSERT INTO tb_category(nome)   VALUES ('iphone');
+INSERT INTO tb_category(nome)   VALUES ('ipad');
